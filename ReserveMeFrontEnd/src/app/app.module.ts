4e04eec1c4b74e7de07856c5f8d001e595f6c398
magyar_adam <magyaradam33@gmail.com>
2020-10-07 06:46:06 +0200

    FEAT: created auth module [issue 4]

--- ReserveMeFrontEnd/src/app/app.module.ts
+++ ReserveMeFrontEnd/src/app/app.module.ts
@@ -3,20 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './auth/login/login.component';
-import { SignUpComponent } from './auth/sign-up/sign-up.component';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-      LoginComponent,
-      SignUpComponent,
-   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, AuthModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
