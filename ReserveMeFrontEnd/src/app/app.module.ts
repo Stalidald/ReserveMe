86390d7f85e8bad6b962cd8ef07b406cb933616f
magyar_adam <magyaradam33@gmail.com>
2020-10-07 06:21:50 +0200

    FEAT: created component for sign-up [issue 4]

--- ReserveMeFrontEnd/src/app/app.module.ts
+++ ReserveMeFrontEnd/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './auth/login/login.component';
+import { SignUpComponent } from './auth/sign-up/sign-up.component';
 
 @NgModule({
-  declarations: [	
+  declarations: [
     AppComponent,
-      LoginComponent
+      LoginComponent,
+      SignUpComponent,
    ],
   imports: [
     BrowserModule,
